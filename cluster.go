f60a1415470f29e78b93e515bce504c0b44ef212
Swapnil Bawaskar <swapnil.bawaskar@rubrik.com>
2026-02-04 17:15:17 +0530

    Implement maxConnLifetime (#9)
    In CAMS we saw that when cqlproxy scaled beyond a single instance,
    all the original connections to the first cqlproxy did not rebalance.
    By setting the lifetime on the connection, we will ensure that the
    connections are created across all the cqlproxy pods.

--- cluster.go
+++ cluster.go
@@ -47,6 +47,7 @@ type ClusterConfig struct {
 	ProtoVersion       int
 	Timeout            time.Duration      // connection timeout (default: 600ms)
 	ConnectTimeout     time.Duration      // initial connection timeout, used during initial dial to server (default: 600ms)
+	ConnMaxLifetime    time.Duration      // SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
 	Port               int                // port (default: 9042)
 	Keyspace           string             // initial keyspace (optional)
 	NumConns           int                // number of connections per host (default: 2)
